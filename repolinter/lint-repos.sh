2672a2183f49671c0239242c1c7509005c86e1e7
Christoph Held <c-held@web.de>
2023-07-24 10:58:05 +0200

    Create lint-repos.sh
    Signed-off-by: Christoph Held <c-held@web.de>

---
+++ repolinter/lint-repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+rm -Rf ../results
+
+# Function to create an issue
+create_issue() {
+  REPO=$1
+  ISSUE_TITLE=$2
+  ISSUE_BODY=$3
+
+  gh issue create -R "$REPO" --title "$ISSUE_TITLE" --body "$ISSUE_BODY"
+}
+
+# List repositories in the organization
+REPOS=$(gh repo list $ORG_NAME -L 100 | awk '{print $1}')
+
+# Loop through each repository and create an issue
+for REPO in $REPOS; do
+  create_issue "$ORG_NAME/$REPO" "Automated Issue Title" "This is the issue body."
+done
