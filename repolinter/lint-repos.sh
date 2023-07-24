d5fc72b6274eee137b51b08ffb79ed3aa70bd82a
Christoph Held <christoph.held@allianz.de>
2023-07-24 15:24:27 +0200

    closing issue

--- repolinter/lint-repos.sh
+++ repolinter/lint-repos.sh
@@ -4,7 +4,7 @@
 issue_exists() {
   REPO=$1
   ISSUE_TITLE=$2
-  gh issue list -R "$REPO" --state open --json title | grep -q "\"title\":\"$ISSUE_TITLE\""
+  gh issue list -R "cheld/org" --state open --json number,title |  jq -r ".[] | select(.title == \"$ISSUE_TITLE\") | .number"
 }
 
 # Function to create an issue if it doesn't already exist
@@ -20,10 +20,19 @@ create_issue_if_not_exists() {
   fi
 }
 
-# List repositories in the organization
-REPOS=$(gh repo list $ORG_NAME --visibility public -L 100 | awk '{print $1}')
+# Function to close an issue
+close_issue() {
+  REPO=$1
+  ISSUE_TITLE=$2
+  ISSUE_NUMBER=$(issue_exists "$REPO" "$ISSUE_TITLE")
+  if [ -n "$ISSUE_NUMBER" ]; then
+    gh issue close -R "$REPO" "$ISSUE_NUMBER"
+    echo "Closed the existing issue with the title '$ISSUE_TITLE' in the repository '$REPO'."
+  fi
+}
 
 # Loop through each repository and create an issue
+REPOS=$(gh repo list $ORG_NAME --visibility public -L 100 | awk '{print $1}')
 for REPO in $REPOS; do
     mkdir -p results/$REPO
     repolinter -g https://github.com/$REPO -f markdown -u https://raw.githubusercontent.com/eBay/.github/main/repolinter.yaml > results/$REPO.md
@@ -31,5 +40,7 @@ for REPO in $REPOS; do
         failure="The repository '$REPO' is not compliant with Allianz guidelines. Please review opensource.allianz.com/guidelines"
         report=`cat results/$REPO.md`
         create_issue_if_not_exists "$REPO" "Repo lint error" "$failure\n\n$report"
+    else
+        close_issue "$REPO" "Repo lint error" 
     fi
 done
