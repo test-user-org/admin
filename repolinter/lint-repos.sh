6b5857f6692861641fe7176dde4ba23ee42c3504
Christoph Held <christoph.held@allianz.de>
2023-07-24 12:54:48 +0200

    fix result creation

--- repolinter/lint-repos.sh
+++ repolinter/lint-repos.sh
@@ -14,7 +14,7 @@ REPOS=$(gh repo list $ORG_NAME --visibility public -L 100 | awk '{print $1}')
 
 # Loop through each repository and create an issue
 for REPO in $REPOS; do
-    mkdir -p ../results/$repo
+    mkdir -p results/$REPO
     repolinter -g https://github.com/$REPO -f markdown -u https://raw.githubusercontent.com/eBay/.github/main/repolinter.yaml > results/$REPO.md
     if [ $? -eq 1 ] ; then
         failure="The repository '$REPO' is not compliant with Allianz guidelines. Please review opensource.allianz.com/guidelines"
