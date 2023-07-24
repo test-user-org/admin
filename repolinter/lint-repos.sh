079ac52a3fa457ea9e3c686c8ff334243e889343
Christoph Held <christoph.held@allianz.de>
2023-07-24 14:53:14 +0200

    only create issue if it does not yet exist

--- repolinter/lint-repos.sh
+++ repolinter/lint-repos.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Function to create an issue
-create_issue() {
+# Function to check if an open issue with the same title already exists
+issue_exists() {
+  REPO=$1
+  ISSUE_TITLE=$2
+  gh issue list -R "$REPO" --state open --json title | grep -q "\"title\":\"$ISSUE_TITLE\""
+}
+
+# Function to create an issue if it doesn't already exist
+create_issue_if_not_exists() {
   REPO=$1
   ISSUE_TITLE=$2
   ISSUE_BODY=$3
-
-  gh issue create -R "$REPO" --title "$ISSUE_TITLE" --body "$ISSUE_BODY"
+  existing_issue_number=$(issue_exists "$REPO" "$ISSUE_TITLE")
+  if [ -z "$existing_issue_number" ]; then
+    gh issue create -R "$REPO" --title "$ISSUE_TITLE" --body "$ISSUE_BODY"
+  else
+    echo "An open issue with the title '$ISSUE_TITLE' already exists in the repository '$REPO'. Skipping issue creation."
+  fi
 }
 
 # List repositories in the organization
@@ -19,6 +30,6 @@ for REPO in $REPOS; do
     if [ $? -eq 1 ] ; then
         failure="The repository '$REPO' is not compliant with Allianz guidelines. Please review opensource.allianz.com/guidelines"
         report=`cat results/$REPO.md`
-        create_issue "$REPO" "Repo lint error" "$failure\n\n$report"
+        create_issue_if_not_exists "$REPO" "Repo lint error" "$failure\n\n$report"
     fi
 done
