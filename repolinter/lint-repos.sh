4bbc8db715af556e0ff7a2b4dac428fab7afaccc
Christoph Held <christoph.held@allianz.de>
2023-07-25 10:30:11 +0200

    fix directories

--- repolinter/lint-repos.sh
+++ repolinter/lint-repos.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+rm -Rf ../results
 
 # Function to check if an open issue with the same title already exists
 issue_exists() {
@@ -34,11 +36,11 @@ close_issue() {
 # Loop through each repository and create an issue
 REPOS=$(gh repo list $ORG_NAME --visibility public -L 100 | awk '{print $1}')
 for REPO in $REPOS; do
-    mkdir -p results/$REPO
-    repolinter -g https://github.com/$REPO -f markdown -u https://raw.githubusercontent.com/eBay/.github/main/repolinter.yaml > results/$REPO.md
+    mkdir -p ../results/$REPO
+    repolinter -g https://github.com/$REPO -f markdown -u https://raw.githubusercontent.com/eBay/.github/main/repolinter.yaml > ../results/$REPO.md
     if [ $? -eq 1 ] ; then
         failure="The repository '$REPO' is not compliant with Allianz guidelines. Please review opensource.allianz.com/guidelines"
-        report=`cat results/$REPO.md`
+        report=`cat ../results/$REPO.md`
         create_issue_if_not_exists "$REPO" "Repo lint error" "$failure\n\n$report"
     else
         close_issue "$REPO" "Repo lint error" 
